314b07f36364e99cbbcdced9e9c586670d19cb7a
khasizadaj <javid.khasizada@gmail.com>
2022-07-28 22:38:37 +0200

    refactor: search_word function
    - checks for word is added, e.g. you cannot search for "5" anymore.
    - easter egg is added for searching for numbers.
    - repetitive codes are removed
    - guard conditions were added to exit from function if needed. This simplified main part of search function.

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,7 @@
 import json
 from difflib import get_close_matches
 from tkinter import Tk, ttk
+from typing import Dict
 
 from config import (
     BODY_FONT,
@@ -15,7 +16,7 @@ from config import (
 
 # import dictionary
 with open("data.json") as dict_file:
-    dictionary = json.load(dict_file)
+    dictionary: Dict = json.load(dict_file)
 
 
 # Window and frames
@@ -69,81 +70,71 @@ means_label_style.configure(
 hemidvs_label = ttk.Label(win, style="Hemidvs.TLabel", text="© made by hemidvs")
 hemidvs_label.place(anchor="n", relx=0.9, rely=0.9)
 
-title_label = ttk.Label(frame1, text="Type word to get mean...", style="Title.TLabel")
+title_label = ttk.Label(frame1, style="Title.TLabel", text="Type word to get mean...")
 title_label.place(relx=0.44, rely=0.08, relwidth=0.80, relheight=0.3, anchor="n")
 
 word_entry = ttk.Entry(frame1, font=ENTRY_FONT)
 word_entry.place(relx=0.32, rely=0.55, relwidth=0.55, relheight=0.2, anchor="n")
 
 
-# Method to search word
-def search_word():
-    word = (word_entry.get()).lower()
+def search_word() -> None:
+    word: str = (word_entry.get()).lower()
 
-    if word != "":
-
-        if word in dictionary:
-            hint_label.config(text="✓ Done!")
-            hint_label_style.configure("Hint.TLabel", foreground=HINT_GREEN_TEXT_COLOR)
-            means = dictionary[word]
-            # if word have more means
-            if type(means) == list:
-                i = 0
-                str_mean = ""
-                while i < len(means):
-                    str_mean = str_mean + f"\n>> {means[i]}"
-                    i = i + 1
-                means_label.config(text=f"{str_mean}")
-            else:
-                means_label.config(text=">> %s" % means)
-
-        elif word.title() in dictionary:
-            hint_label.config(text="✓ Done!")
-            hint_label_style.configure("Hint.TLabel", foreground=HINT_GREEN_TEXT_COLOR)
-            means = dictionary[word.title()]
-            # if word have more means
-            if type(means) == list:
-                i = 0
-                str_mean = ""
-                while i < len(means):
-                    str_mean = str_mean + f"\n>> {means[i]}"
-                    i = i + 1
-                means_label.config(text=f"{str_mean}")
-            else:
-                means_label.config(text=">> %s" % means)
-
-        elif word.upper() in dictionary:
-            hint_label.config(text="✓ Done!")
-            hint_label_style.configure("Hint.TLabel", foreground=HINT_GREEN_TEXT_COLOR)
-            means = dictionary[word.upper()]
-            # if word have more means
-            if type(means) == list:
-                i = 0
-                str_mean = ""
-                while i < len(means):
-                    str_mean = str_mean + f"\n>> {means[i]}"
-                    i = i + 1
-                means_label.config(text=f"{str_mean}")
-            else:
-                means_label.config(text=">> %s" % means)
-        else:
-            hint_label.config(text="✗ We can't find your word, Please type correct.")
-            hint_label_style.configure("Hint.TLabel", foreground=HINT_RED_TEXT_COLOR)
-            means_label.config(text="")
-    else:
-        hint_label.config(text="✗ Oops.. You can't search noting!")
+    if word == "":
+        hint_label.config(text="✗ Oops.. You can't search nothing!")
         hint_label_style.configure("Hint.TLabel", foreground=HINT_RED_TEXT_COLOR)
+        return
+
+    if word.isnumeric() and word != "42":
+        hint_label.config(
+            text="✗ Sorry, you can't search for a number! Only number that has meaning is 42. Check that out!"
+        )
+        hint_label_style.configure("Hint.TLabel", foreground=HINT_RED_TEXT_COLOR)
+        return
+
+    if word == "42":
+        hint_label.config(text="✓ Are you looking for a meaning of your life?")
+        hint_label_style.configure("Hint.TLabel", foreground=HINT_GREEN_TEXT_COLOR)
+        means_label.config(
+            text='>> 42 is the number from which all meaning ("the meaning of life, the universe, and everything") can be derived.'
+        )
+        return
+
+    if word in dictionary:
+        meaning = dictionary[word]
+    elif word.title() in dictionary:
+        meaning = dictionary[word.title()]
+    elif word.upper() in dictionary:
+        meaning = dictionary[word.upper()]
+    else:
+        meaning = ""
+
+    if meaning == "":
+        hint_label.config(text="✗ We can't find your word, Please type correct.")
+        hint_label_style.configure("Hint.TLabel", foreground=HINT_RED_TEXT_COLOR)
+        # reset meaning label
+        means_label.config(text="")
+        return
+
+    hint_label.config(text="✓ Done!")
+    hint_label_style.configure("Hint.TLabel", foreground=HINT_GREEN_TEXT_COLOR)
+
+    # if word have more meanings, we combine it in one string
+    if isinstance(meaning, list):
+        meaning = "\n>> ".join(meaning)
+
+    means_label.config(text=f">> {meaning}")
 
 
 # other Widgets
 search_button = ttk.Button(frame1, text="Search", command=search_word)
 search_button.place(relx=0.72, rely=0.55, relwidth=0.18, relheight=0.2, anchor="n")
 
-hint_label = ttk.Label(frame1, text="", style="Hint.TLabel")
-hint_label.place(relx=0.05, rely=0.80, anchor="nw")
+hint_label = ttk.Label(frame2, text="", style="Hint.TLabel")
+hint_label.place(anchor="nw")
 
 means_label = ttk.Label(frame2, style="Means.TLabel", wraplength=630)
-means_label.place(anchor="nw")
+means_label.place(rely=0.15, anchor="nw")
 
 # def translate(word):
 #     word = word.lower()
