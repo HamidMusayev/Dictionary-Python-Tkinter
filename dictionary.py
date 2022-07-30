c894f188325344bc7ff542ba9368f8813d54fbbb
khasizadaj <javid.khasizada@gmail.com>
2022-07-30 03:27:24 +0200

    chore: clean up code

--- dictionary.py
+++ dictionary.py
@@ -19,64 +19,6 @@ with open("data.json") as dict_file:
     dictionary: Dict = json.load(dict_file)
 
 
-# Window and frames
-win = Tk()
-win.title("Dictionary")
-win.geometry("750x400")
-win.config(bg=WINDOW_COLOR)
-win.resizable(False, False)
-
-frame1 = ttk.Frame(win, style="Frame.TFrame")
-frame1.place(relx=0.34, rely=0.08, relwidth=0.6, relheight=0.4, anchor="n")
-
-frame2 = ttk.Frame(win, style="Frame.TFrame")
-frame2.place(relx=0.49, rely=0.45, relwidth=0.85, relheight=0.5, anchor="n")
-
-# Styles
-hemidvs_style = ttk.Style()
-hemidvs_style.configure(
-    "Hemidvs.TLabel", font=COPYRIGHT_FONT, foreground="white", background=WINDOW_COLOR
-)
-
-frames_style = ttk.Style()
-frames_style.configure("Frame.TFrame", background=WINDOW_COLOR)
-
-title_label_style = ttk.Style()
-title_label_style.configure(
-    "Title.TLabel",
-    font=TITLE_FONT,
-    foreground="white",
-    background=WINDOW_COLOR,
-)
-
-hint_label_style = ttk.Style()
-hint_label_style.configure(
-    "Hint.TLabel",
-    font=BODY_FONT,
-    foreground=HINT_GREEN_TEXT_COLOR,
-    background=WINDOW_COLOR,
-)
-
-means_label_style = ttk.Style()
-means_label_style.configure(
-    "Means.TLabel",
-    font=BODY_FONT,
-    foreground=MEANS_TEXT_COLOR,
-    background=WINDOW_COLOR,
-)
-
-
-# Widgets
-hemidvs_label = ttk.Label(win, style="Hemidvs.TLabel", text="© made by hemidvs")
-hemidvs_label.place(anchor="n", relx=0.9, rely=0.9)
-
-title_label = ttk.Label(frame1, style="Title.TLabel", text="Type word to get mean...")
-title_label.place(relx=0.44, rely=0.08, relwidth=0.80, relheight=0.3, anchor="n")
-
-word_entry = ttk.Entry(frame1, font=ENTRY_FONT)
-word_entry.place(relx=0.32, rely=0.55, relwidth=0.55, relheight=0.2, anchor="n")
-
-
 def main() -> None:
     word = word_entry.get().lower()
     print(word)
@@ -144,55 +86,76 @@ def search(word: str) -> Dict:
     return result
 
 
-# other Widgets
-search_button = ttk.Button(frame1, text="Search", command=main)
-
-search_button.place(relx=0.72, rely=0.55, relwidth=0.18, relheight=0.2, anchor="n")
-
-hint_label = ttk.Label(frame2, text="", style="Hint.TLabel")
-hint_label.place(anchor="nw")
-
-means_label = ttk.Label(frame2, style="Means.TLabel", wraplength=630)
-means_label.place(rely=0.15, anchor="nw")
-
-
 if __name__ == "__main__":
+    # Window and frames
+    win = Tk()
+    win.title("Dictionary")
+    win.geometry("750x400")
+    win.config(bg=WINDOW_COLOR)
+    win.resizable(False, False)
+
+    frame1 = ttk.Frame(win, style="Frame.TFrame")
+    frame1.place(relx=0.34, rely=0.08, relwidth=0.6, relheight=0.4, anchor="n")
+
+    frame2 = ttk.Frame(win, style="Frame.TFrame")
+    frame2.place(relx=0.49, rely=0.45, relwidth=0.85, relheight=0.5, anchor="n")
+
+    # Styles
+    hemidvs_style = ttk.Style()
+    hemidvs_style.configure(
+        "Hemidvs.TLabel",
+        font=COPYRIGHT_FONT,
+        foreground="white",
+        background=WINDOW_COLOR,
+    )
+
+    frames_style = ttk.Style()
+    frames_style.configure("Frame.TFrame", background=WINDOW_COLOR)
+
+    title_label_style = ttk.Style()
+    title_label_style.configure(
+        "Title.TLabel",
+        font=TITLE_FONT,
+        foreground="white",
+        background=WINDOW_COLOR,
+    )
+
+    hint_label_style = ttk.Style()
+    hint_label_style.configure(
+        "Hint.TLabel",
+        font=BODY_FONT,
+        foreground=HINT_GREEN_TEXT_COLOR,
+        background=WINDOW_COLOR,
+    )
+
+    means_label_style = ttk.Style()
+    means_label_style.configure(
+        "Means.TLabel",
+        font=BODY_FONT,
+        foreground=MEANS_TEXT_COLOR,
+        background=WINDOW_COLOR,
+    )
+
+    # Widgets
+    hemidvs_label = ttk.Label(win, style="Hemidvs.TLabel", text="© made by hemidvs")
+    hemidvs_label.place(anchor="n", relx=0.9, rely=0.9)
+
+    title_label = ttk.Label(
+        frame1, style="Title.TLabel", text="Type word to get mean..."
+    )
+    title_label.place(relx=0.44, rely=0.08, relwidth=0.80, relheight=0.3, anchor="n")
+
+    word_entry = ttk.Entry(frame1, font=ENTRY_FONT)
+    word_entry.place(relx=0.32, rely=0.55, relwidth=0.55, relheight=0.2, anchor="n")
+
+    # other Widgets
+    search_button = ttk.Button(frame1, text="Search", command=main)
+
+    search_button.place(relx=0.72, rely=0.55, relwidth=0.18, relheight=0.2, anchor="n")
+
+    hint_label = ttk.Label(frame2, text="", style="Hint.TLabel")
+    hint_label.place(anchor="nw")
+
+    means_label = ttk.Label(frame2, style="Means.TLabel", wraplength=630)
+    means_label.place(rely=0.15, anchor="nw")
     win.mainloop()
-
-
-# TODO uncomment this function to help user find out word options while typing
-# def translate(word):
-#     word = word.lower()
-#     closes = get_close_matches(word, data.keys())
-#
-#     if word in data:
-#         return data[word]
-#     elif word.title() in data:
-#         return data[word.title()]
-#     elif word.upper() in data:
-#         return data[word.upper()]
-#
-#     elif len(closes) > 0:
-#         print("Did you mean %s?" % closes[0])
-#         decide = input("Press y for yes or n for no - ")
-#         if decide == "y":
-#             return data[closes[0]]
-#         else:
-#             print("Did you mean %s?" % closes[1])
-#             decide2 = input("Press y for yes or n for no - ")
-#             if decide2 == "y":
-#                 return data[closes[1]]
-#             else:
-#                 return "Word isn't exits in dictionary"
-#
-#     else:
-#         return "Word isn't exits in dictionary"
-#
-#
-# word = input("Enter the word you want to search: ")
-# output = translate(word)
-# if type(output) == list:
-#     for i in output:
-#         print(i)
-# else:
-#     print(output)
